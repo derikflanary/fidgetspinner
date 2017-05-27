b8ab88bb0b9757561eb3b97542791d552d9cc1ba
Derik Flanary <derikflanary@gmail.com>
2017-05-26 19:40:42 -0600

    spins on flick

--- fidgetSpinner/ViewController.swift
+++ fidgetSpinner/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreMotion
+import SpriteKit
 
 class ViewController: UIViewController {
     
@@ -16,8 +17,11 @@ class ViewController: UIViewController {
     var isSpinningClockwise = true
     var spinningStarted = false
     var spinCount = 0
+    var previousDegrees: Double = 0
+    var scene = SKScene()
+    var spinnerNode = SKSpriteNode()
 
-    @IBOutlet weak var spinnerView: UIView!
+    @IBOutlet weak var spinnerView: SKView!
     @IBOutlet weak var xLabel: UILabel!
     @IBOutlet weak var yLabel: UILabel!
     @IBOutlet weak var zLabel: UILabel!
@@ -29,6 +33,25 @@ class ViewController: UIViewController {
         configureMotionManager()
         spinnerView.layer.cornerRadius = 5
         spinnerView.clipsToBounds = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        scene = SKScene(size: spinnerView.frame.size)
+        scene.backgroundColor = UIColor.white
+        spinnerView.presentScene(scene)
+        spinnerNode = SKSpriteNode(color: UIColor.gray, size: CGSize(width: spinnerView.frame.width/3, height: spinnerView.frame.height/3))
+        spinnerNode.anchorPoint = CGPoint.zero
+        spinnerNode.physicsBody = SKPhysicsBody(rectangleOf: spinnerNode.size)
+        spinnerNode.physicsBody?.affectedByGravity = false
+        spinnerNode.physicsBody?.angularDamping = 0.75
+        spinnerView.scene?.addChild(spinnerNode)
+        spinnerNode.position = CGPoint(x: scene.frame.width/2, y: scene.frame.height/2)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        motionManager.stopDeviceMotionUpdates()
     }
     
 
@@ -44,25 +67,37 @@ private extension ViewController {
     func configureMotionManager() {
         motionManager = CMMotionManager()
         if motionManager.isDeviceMotionAvailable {
-            motionManager.deviceMotionUpdateInterval = 0.01
-            motionManager.startDeviceMotionUpdates(to: .main, withHandler: { (data, error) in
+            motionManager.deviceMotionUpdateInterval = 0.1
+            let queue = OperationQueue()
+            motionManager.startDeviceMotionUpdates(to: queue, withHandler: { (data, error) in
                 guard let data = data else { return }
                 
                 if let initialAttitude = self.initialAttitude {
                     // translate the attitude
                     data.attitude.multiply(byInverseOf: initialAttitude)
-                    
-                    self.xLabel.text = "roll: \(data.attitude.roll.degrees())"
-                    self.yLabel.text = "yaw: \(data.attitude.yaw.degrees())"
-                    
-                    
+                    let yawDegrees = data.attitude.yaw.degrees()
                     let rotation = atan2(data.gravity.x, data.gravity.y) - Double.pi
-                    self.zLabel.text = "\(rotation)"
                     
-//                    self.spinnerView.transform = CGAffineTransform(rotationAngle: CGFloat(rotation))
-                    self.spinnerView.transform = CGAffineTransform(rotationAngle: CGFloat(data.attitude.yaw))
+                    if yawDegrees + self.previousDegrees >= 360 && yawDegrees > self.previousDegrees {
+                        self.spinCount += 1
+                        self.previousDegrees = 0
+                    }
                     
-                    
+                    OperationQueue.main.addOperation {
+//                        self.spinnerView.transform = CGAffineTransform(rotationAngle: CGFloat(data.attitude.yaw))
+//                        self.zLabel.text = "spins: \(self.spinCount)"
+//                        self.xLabel.text = "previous: \(self.previousDegrees)"
+//                        self.yLabel.text = "yaw: \(yawDegrees)"
+                        
+                        self.xLabel.text = "acceleration: \(data.userAcceleration.x)"
+                        if data.userAcceleration.x < -1.5 {
+                            self.spinnerNode.physicsBody?.applyAngularImpulse(CGFloat(data.userAcceleration.x))
+                        }
+                    }
+
+                    if yawDegrees > self.previousDegrees && self.previousDegrees - yawDegrees < 20 {
+                        self.previousDegrees = yawDegrees
+                    }
                 } else {
                     self.initialAttitude = self.motionManager.deviceMotion?.attitude
                 }
