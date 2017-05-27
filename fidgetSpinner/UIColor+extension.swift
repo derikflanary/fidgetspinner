14eb36880f30a5ccee71e4c8941c10e62c5915bd
Derik Flanary <derikflanary@gmail.com>
2017-05-27 14:51:26 -0600

    add stop and restart buttons

---
+++ fidgetSpinner/UIColor+extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+extension.swift
+//  fidgetSpinner
+//
+//  Created by Derik Flanary on 5/27/17.
+//  Copyright © 2017 Derik Flanary. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    
+    
+}
--- fidgetSpinner/ViewController.swift
+++ fidgetSpinner/ViewController.swift
@@ -16,24 +16,33 @@ class ViewController: UIViewController {
     var spinCount = 0
     var scene = SKScene()
     var spinnerNode = SKSpriteNode()
+    var circleNode = SKShapeNode()
+    var totalDegrees: Double = 0
+    var previousDegrees: Double = 0
 
     @IBOutlet weak var spinnerView: SKView!
     @IBOutlet weak var xLabel: UILabel!
     @IBOutlet weak var yLabel: UILabel!
-    @IBOutlet weak var zLabel: UILabel!
+    @IBOutlet weak var circleView: UIView!
+    @IBOutlet weak var topView: UIView!
     
-    @IBOutlet weak var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureMotionManager()
         spinnerView.layer.cornerRadius = 5
         spinnerView.clipsToBounds = true
+        xLabel.text = ""
+        circleView.backgroundColor = UIColor(red: 0.5, green: 0.69, blue: 0.86, alpha: 1.0)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         setupScene()
+        circleView.layer.cornerRadius = circleView.frame.width/2
+        circleView.clipsToBounds = true
+        topView.layer.cornerRadius = 10
+        topView.clipsToBounds = true
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -41,6 +50,20 @@ class ViewController: UIViewController {
         motionManager.stopDeviceMotionUpdates()
     }
     
+    @IBAction func stopButtonTapped() {
+        spinnerNode.physicsBody?.angularVelocity = 0
+        motionManager.stopDeviceMotionUpdates()
+        configureMotionManager()
+    }
+    
+    @IBAction func restartButtonTapped() {
+        spinnerNode.physicsBody?.angularVelocity = 0
+        motionManager.stopDeviceMotionUpdates()
+        spinCount = 0
+        totalDegrees = 0
+        previousDegrees = 0
+        configureMotionManager()
+    }
 }
 
 private extension ViewController {
@@ -56,17 +79,13 @@ private extension ViewController {
         spinnerNode = SKSpriteNode(color: UIColor.gray, size: CGSize(width: spinnerView.frame.width/1.2, height: spinnerView.frame.height/1.2))
         spinnerNode.physicsBody = SKPhysicsBody(rectangleOf: spinnerNode.size)
         spinnerNode.physicsBody?.affectedByGravity = false
-        spinnerNode.physicsBody?.angularDamping = 0.85
-        spinnerNode.physicsBody?.mass = 10
+        spinnerNode.physicsBody?.angularDamping = 0.3
+        spinnerNode.physicsBody?.mass = 1.5
         spinnerNode.anchorPoint = CGPoint(x: 0.5, y: 0.42)
         let texture = SKTexture(image: #imageLiteral(resourceName: "fidget"))
         spinnerNode.texture = texture
         spinnerNode.position = CGPoint(x: scene.frame.width/2, y: scene.frame.height/2)
-        
-        let circleNode = SKShapeNode(circleOfRadius: spinnerView.frame.width/10)
-        circleNode.fillColor = UIColor.red
-        circleNode.position = CGPoint(x: scene.frame.width/2, y: scene.frame.height/2)
-        
+
         spinnerView.scene?.addChild(spinnerNode)
         spinnerView.scene?.addChild(circleNode)
     }
@@ -74,25 +93,32 @@ private extension ViewController {
     func configureMotionManager() {
         motionManager = CMMotionManager()
         if motionManager.isDeviceMotionAvailable {
-            motionManager.deviceMotionUpdateInterval = 0.02
+            motionManager.deviceMotionUpdateInterval = 0.01
             let queue = OperationQueue()
             motionManager.startDeviceMotionUpdates(to: queue, withHandler: { data, error in
                 guard let data = data else { return }
-                
+                let zRotation = Double(self.spinnerNode.zRotation)
+                let degrees = zRotation.degrees()
+                if degrees - self.previousDegrees > 0 {
+                    self.totalDegrees += (degrees - self.previousDegrees)
+                }
+
                 OperationQueue.main.addOperation {
-                    
                     if data.userAcceleration.x < -0.2 {
-                        self.spinnerNode.physicsBody?.applyAngularImpulse(CGFloat(data.userAcceleration.x) * 50)
+                        self.spinnerNode.physicsBody?.applyAngularImpulse(CGFloat(data.userAcceleration.x) * -1)
                     }
                     if let physicsBody = self.spinnerNode.physicsBody {
-                        let mph = abs(Int(physicsBody.angularVelocity * 2.24))
-                        self.yLabel.text = "\(mph) MPH"
+                        let rpm = abs(Int(physicsBody.angularVelocity * 30/CGFloat.pi))
+                        
+                        let diff: CGFloat = (CGFloat(rpm) * 0.001)
+                        self.circleView.backgroundColor = UIColor(red: 0.5 + diff, green: 0.69 + diff, blue: 0.86, alpha: 1.0)
+                        self.xLabel.text = "\(rpm)/min"
                     }
                     
-                    self.xLabel.text = "\(self.spinnerNode.zRotation)"
-                    self.zLabel.text = ""
+                    let spins = self.totalDegrees / 360
+                    self.yLabel.text = String(format: "%.0f Spins" , arguments: [spins])
                 }
-                
+                self.previousDegrees = degrees
             })
         } else {
             print("no motion")
